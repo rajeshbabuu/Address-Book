d7f2e056ec96149d6ef73a37ed4df63568a46453
Rajesh kumar <rajeshbabu656656@gmail.com>
2023-01-10 20:37:15 +0530

    Welcome Address Book and Add person Details

---
+++ AddressBook/Person.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBook
+{
+    internal class Person
+    {
+        public string? FirstName { get; set; }  
+        public string? LastName { get; set; }
+        public string? Email { get; set; }
+        public string? Address { get; set; }
+        public string? City { get; set; }
+        public long MobileNumber { get; set; }
+        public string? State { get; set; }
+         public int? zip { get; set; }
+
+        }
+    }
+    
+
---
+++ AddressBook/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.IO.Pipes;
+
+namespace AddressBook
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Address Book");
+            Person person = new Person();
+
+            person.FirstName = "Rajesh";
+            person.LastName = "Kumar";
+            person.MobileNumber = 455555;
+            person.Email = "rajesh007@gmail.com";
+            person.Address = " Moti Zheel Etawah";
+            person.City = "Etawah";
+            person.zip = 123456;
+            person.State = "UP";
+        }
+    }
+}
